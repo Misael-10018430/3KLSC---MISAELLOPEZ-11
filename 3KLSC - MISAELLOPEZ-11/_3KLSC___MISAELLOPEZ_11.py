2573c4f59913c0b469016d53b77af42975516f22
MISAEL L.A. <misa690la90@outlook.com>
2024-10-07 19:57:59 -0600

    Agregar archivos de proyecto.

---
+++ 3KLSC - MISAELLOPEZ-11/_3KLSC___MISAELLOPEZ_11.py
@@ -0,0 +1,10 @@
+
+num = int(input("Digite numero: "))
+i = 1
+
+while i <= 10:
+    resultado = num * i
+    print(f"{num} x {i} = {resultado}")
+    i += 1
+
+input("Pulse una tecla para terminar: ")
